a1730e0531a0e96fd1b2e16de8b3c49bdafae7a3
oAllanWeb <allanweb019@gmail.com>
2021-10-24 16:04:29 -0300

    :zap: teste novo deploy

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// Replace `nextjs-github-pages` with your Github repo project name.
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/devcia.github.io/' : '',
+}
