997688854f76aa6dacb94d5cad786d04bfa952a0
oAllanWeb <oAllanWeb@users.noreply.github.com>
2021-10-26 03:03:56 +0000

    deploy: e8573ebc1aec14336bb231a6af10a24f49cbbf03

---
+++ _next/static/jAkp1atTPQIh7bVoSIfD2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/62801c0c276f9c828d07.css","static/chunks/pages/index-d8d9be348b1d79b7a2d3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
